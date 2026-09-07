b65dbd8a01c96fb72db605e6a520d901b465abdc
Radduso Léna <lena@MacBookAir.lan>
2026-09-07 10:41:54 +0200

    Correction configuration administration Grand Plus

--- public/grand-plus-admin.php
+++ public/grand-plus-admin.php
@@ -45,7 +45,7 @@ const WINNER_FILE =
     __DIR__ . '/grand-plus-winner.json';
 
 const CONFIG_FILE =
-    __DIR__ . '/vitrine-mail-config.php';
+    dirname(__DIR__) . '/vitrine-mail-config.php';
 
 const UNSUBSCRIBE_FILE =
     DATA_DIR . '/unsubscribed.json';
--- public/grand-plus-unsubscribe.php
+++ public/grand-plus-unsubscribe.php
@@ -25,7 +25,7 @@ const UNSUBSCRIBE_FILE =
     DATA_DIR . '/unsubscribed.json';
 
 const CONFIG_FILE =
-    __DIR__ . '/vitrine-mail-config.php';
+    dirname(__DIR__) . '/vitrine-mail-config.php';
 
 const DEFAULT_SITE_URL =
     'https://vitrineplus.fr';
